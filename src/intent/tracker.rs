0537346d9bff01b06d53d24414158e2b7be366c8
SMOL <smolnero@gmail.com>
2025-06-04 13:41:55 -0600

    IP

--- src/intent/tracker.rs
+++ src/intent/tracker.rs
@@ -2,8 +2,11 @@ use std::collections::Hashmap
 
 // Each intent holds a unique label and whether its been acted on
 pub struct Intent {
-	pub label: String,
-	pub completed: bool,
+	pub label: String,		          // A name or description of the intent
+	pub completed: bool,	          // When the intent was recorded 
+    pub value: f32,			          // Optional weight or intesisty
 }
 
-// The tracker keesps track of all intentions
+pub struct IntentTracker {
+	intents: HasMap<String, Intent>,  // Using a HashMap for label-based access
+}
