214490d3ddab60ab0b10b285b6461098bbc1308a
SMOL <smolnero@gmail.com>
2025-06-04 13:53:03 -0600

    Trackers (understand)

--- src/intent/tracker.rs
+++ src/intent/tracker.rs
@@ -1,13 +1,27 @@
-use std::collections::Hashmap
+use std::collections::HashMap;
 
 // Each intent holds a unique label and whether its been acted on
 pub struct Intent {
 	pub label: String,		          // A name or description of the intent
-	pub completed: bool,	          // When the intent was recorded 
+	pub completed: bool,	          // When the intent was recorded (ISO 8601)
     pub value: f32,			          // Optional weight or intesisty
 }
 
 // Core struct for storing and managing ALL recorded intents
 pub struct IntentTracker {
 	intents: HashMap<String, Intent>,  // Using a HashMap for label-based access
+}
+
+// Creates new empty tracker 
+impl IntentTracker {
+	pub fn new() -> Self {
+		Self {
+			intents: HashMap::new(),
+		}
+	}
+
+
+
+
+
 }
